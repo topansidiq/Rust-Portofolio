3948b2981874d92aa6c7faaa764340ec0a93aae3
topansidiq <topansidiq@gmail.com>
2025-02-15 22:22:26 +0700

    Update belajar-rust-dasar

--- belajar-rust-dasar/src/main.rs
+++ belajar-rust-dasar/src/main.rs
@@ -6,3 +6,56 @@ fn main() {
 fn hello_test(){
     println!("Hello, Test!");
 }
+#[test]
+fn test_variable(){
+    let name = "Topan Sidiq";
+    println!("Hello, {}", name);
+}
+#[test]
+fn test_mutable(){
+    let mut x = 5;
+    println!("x is {}", x);
+    x = 6;
+    println!("x is {}", x);
+}
+
+#[test]
+fn static_typing(){
+    let x = 5;
+    println!("x is {}", x);
+    // x = "Topan"; Error
+}
+
+#[test]
+fn shadowing(){
+    let name = "Topan Sidiq";
+    println!("Hello, {}", name);
+
+    let name = "Salsabila Agustin";
+    println!("Hello, {}", name);
+}
+
+// This is comment
+
+/*
+    This is multiple line comment
+ */
+
+#[test]
+fn explicit(){
+    let x = 5;
+    let y = 5;
+    println!("x is {}", x);
+    println!("y is {}", y);
+    println!("x + y = {} + {}\n\t  = {}", x, y, x + y);
+}
+
+#[test]
+fn number(){
+    let a = 10;
+    println!("a is {}", a);
+    let b = 10.5;
+    println!("b is {}", b);
+
+    println!("a + b = {} + {}", a, b);
+}
